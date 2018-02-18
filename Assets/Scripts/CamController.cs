2f639f7861430f4d6b3b1e47e99cd5e4091c6ae5
Justin <justified-1@hotmail.com>
2018-02-18 15:59:13 -0700

    Added initial camera tracker and build colliders for stage

---
+++ Assets/Scripts/CamController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour {
+
+	public GameObject Male;
+
+	//offset is the difference 
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - Male.transform.position;
+	}
+	
+	//Runs every frame just like update, but it runs after all other items run for update
+	//Thus camera in sure to move AFTER character has moved
+	void LateUpdate () {
+		transform.position = Male.transform.position + offset;
+	}
+}
