038a73c15e8c48d87134da2eeb23661b4e5c0fb4
Justin <justified-1@hotmail.com>
2018-03-04 01:25:47 -0700

    Created Mr goo character with several animations

--- Assets/Scripts/CamController.cs
+++ Assets/Scripts/CamController.cs
@@ -8,14 +8,17 @@ public class CamController : MonoBehaviour {
 
 	//offset is the difference 
 	private Vector3 offset;
+		//private Vector3 zStopper;
 
 	// Use this for initialization
 	void Start () {
 		offset = transform.position - Male.transform.position;
+		//zStopper = Mathf.Clamp(Male.transform.position.z, 0, 0);
 	}
 	
 	//Runs every frame just like update, but it runs after all other items run for update
 	//Thus camera in sure to move AFTER character has moved
+
 	void LateUpdate () {
 		transform.position = Male.transform.position + offset;
 	}
