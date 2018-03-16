161f2639aa169cdd5843d4e8bac5a2412cf088fc
Jim Chavez <jimstanley.chavez@umontana.edu>
2018-03-15 22:51:19 -0600

    Added HUD
    Added a HUD to the Casino Room Level.  The player names are not working now for some reason.

---
+++ Assets/Scripts/Entities/BattleController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BattleController : MonoBehaviour
+{
+    public int roundTime = 99;
+    private float lastTimeUpdate = 0;
+
+    public HUDController hud;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(roundTime > 0 && Time.time - lastTimeUpdate > 1)
+        {
+            roundTime--;
+            lastTimeUpdate = Time.time;
+        }
+
+        //if(roundTime < 90)
+        //{
+        //    if (hud.leftBar.size < hud.rightBar.size)
+        //    {
+        //        SceneManager.LoadScene(0);
+        //    }
+        //}
+	}
+}
